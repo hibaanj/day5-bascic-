09445742b54613b5e7cc4e1931fd56ffa4d487c1
hibaanj <anjumhiba15@gmail.com>
2022-07-17 18:18:40 +0530

    coin flip heads and tails program

---
+++ 5-UC1coinflip/5-UC1coinflip/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Coreprogram
+{
+    public class Coin
+    {
+        public void Coinflip()
+        {
+            double head =0, tail = 0,limit = 0.5;
+            Console.WriteLine("enter the times you want to flip the coin");
+            int flip = Convert.ToInt32(Console.ReadLine());
+            if(flip > 0)
+            {
+                for(int i =0; i<=flip; i++)
+                {
+                    Random rn = new Random();
+                    int output = rn.Next(0, 2);
+                    //Console.WriteLine(output);
+                    if (output < 0.5)
+                    {
+                        //Console.WriteLine("tails");
+                        tail++;
+                    }
+                    else
+                    {
+
+                        //Console.WriteLine("head");
+                        head++;
+                    }
+                }
+                //Console.WriteLine( head);
+                //Console.WriteLine(tail);
+                double prehead = (head / flip) * 100;
+                double pretail = (tail / flip) * 100;
+                Console.WriteLine($"the head percentage is {prehead} %");
+                Console.WriteLine($"the tail percentage is {pretail} %");
+            }
+            else
+            {
+                Console.WriteLine("invalid number");
+            }
+            
+
+        }
+        public static void Main(string[] args)
+        {
+            Coin c = new Coin();
+            c.Coinflip();
+            
+        }
+    }
+}
