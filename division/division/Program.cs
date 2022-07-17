86f9aa71c2df82b0df0442ac2a2be79fdda62af6
hibaanj <anjumhiba15@gmail.com>
2022-07-17 19:58:52 +0530

    finding remainder and quotient

---
+++ division/division/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Division
+{
+    public void Remquo()
+    {
+        Console.WriteLine("enter the dividend");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter the divisor");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        if ((a > 0) && (b > 0))
+        {
+            Console.WriteLine($"The quotient is {a / b}");
+            Console.WriteLine($"The remainder is {a % b}");
+        }
+        else
+        {
+            Console.WriteLine("Invalud input, negative integers are not allowed");
+        }
+    }
+    public static void Main(string[] args){
+        Division dev = new Division();
+        dev.Remquo();
+    }
+}
