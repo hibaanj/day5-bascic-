412b381311f7299ca7badee8dfdfc5a2277b0331
hibaanj <anjumhiba15@gmail.com>
2022-07-17 20:41:49 +0530

    vowels or consonents

---
+++ vowels/vowels/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Vowels
+{
+    public static void Vowelss()
+    {
+        Console.WriteLine("Enter a letter to check whether it is vowel");
+        char n = Convert.ToChar(Console.ReadLine());
+        n = char.ToUpper(n);
+        if (n == 'A' || n == 'E' || n == 'I' || n == 'O' || n == 'U')
+        {
+            Console.WriteLine($"The letter {n} is a vowel");
+        }
+        else
+        {
+            Console.WriteLine($"The letter {n} is a consonant");
+        }
+
+    }
+    public static void Main(string[] args)
+    {
+        Vowelss();
+    }
+}
