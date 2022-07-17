5d8b994d00183343b653fc72fed8fba2fda2236a
hibaanj <anjumhiba15@gmail.com>
2022-07-17 19:04:28 +0530

    power of 2 problem

---
+++ twospower/twospower/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace day5
+{
+    public class Power
+    {
+        public static void Power2()
+
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int limit = 31, num = 0;
+            if (n <= limit)
+            {
+                while (num <= n)
+                {
+                    double power = Math.Pow(2, num);
+                    Console.WriteLine(power);
+                    num++;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Invalid input, limit is {limit}");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Power2();
+        }
+    }
+}
