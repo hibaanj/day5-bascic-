46f26aff9a0e1f40119ef262d1ccafcae265acd5
hibaanj <anjumhiba15@gmail.com>
2022-07-17 20:21:03 +0530

    checking whether a number is evenn or odd

---
+++ evenorodd/evenorodd/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Evenodd
+{
+    public static void Evnodd()
+    {
+        Console.WriteLine("Enter a numbers to check odd or even");
+        int a = Convert.ToInt32(Console.ReadLine());
+        if (a % 2 == 0)
+        {
+            Console.WriteLine($"The number is even");
+        }
+        else
+        {
+            Console.WriteLine($"The number is odd");
+        }
+
+    }
+    public static void Main(string[] args)
+    {
+        Evnodd();
+    }
+}
