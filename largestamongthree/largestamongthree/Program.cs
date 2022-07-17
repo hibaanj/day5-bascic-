59fea52e26502a163d0dd9e4d3aba3dc6f6714e6
hibaanj <anjumhiba15@gmail.com>
2022-07-17 20:48:11 +0530

    largest among three

---
+++ largestamongthree/largestamongthree/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Largest
+{
+    public void Largestnumber()
+    {
+        Console.WriteLine("Enter three numbers");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+        var largest = a > b ? (a > c ? a : c) : (b > c ? b : c);
+        Console.WriteLine($"the largest numbeer is {largest} ");
+    }
+    public static void Main(string[] args)
+    {
+        Largest number = new Largest();
+        number.Largestnumber();
+
+    }
+}
